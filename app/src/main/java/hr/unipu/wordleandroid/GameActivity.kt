3418cd366245570f52e08d6605a5f4a57e152761
David <60017346+DavidMaglica@users.noreply.github.com>
2023-05-27 23:11:47 +0200

    Squashed commit of the following:
    commit b7282ee782c85003f4552483d97f9534ab0886d3
    Author: David <60017346+DavidMaglica@users.noreply.github.com>
    Date:   Sat May 27 23:10:30 2023 +0200
    
        Reading from .txt files

--- app/src/main/java/hr/unipu/wordleandroid/GameActivity.kt
+++ app/src/main/java/hr/unipu/wordleandroid/GameActivity.kt
@@ -7,25 +7,83 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class GameActivity : AppCompatActivity() {
     lateinit var keyboardLayout: LinearLayout
     lateinit var helpButton: ImageButton
     lateinit var restartButton: ImageButton
 
+    private fun initiateDictionaryWords(): MutableList<String> {
+        val dictionaryWordsList: MutableList<String> = mutableListOf()
+
+        val dictionaryStringBuilder = StringBuilder()
+        val dictionaryInputStream: InputStream = this.resources.openRawResource(R.raw.dictionary)
+        val dictionaryReader = BufferedReader(InputStreamReader(dictionaryInputStream))
+
+        while (true) {
+            var dictionaryWord: String? = null
+            try {
+                dictionaryWord = dictionaryReader.readLine()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            if (dictionaryWord == null) break
+            dictionaryStringBuilder.append(dictionaryWord).append("\n")
+        }
+        dictionaryInputStream.close()
+
+        val completeString = dictionaryStringBuilder.toString()
+        dictionaryWordsList.addAll(completeString.split("\n"))
+        return dictionaryWordsList
+    }
+
+    private fun initiateWinningWords(): MutableList<String> {
+        val winningWordsList: MutableList<String> = mutableListOf()
+
+        val winningStringBuilder = StringBuilder()
+        val winningInputStream: InputStream = this.resources.openRawResource(R.raw.winningwords)
+        val winningReader = BufferedReader(InputStreamReader(winningInputStream))
+
+        while (true) {
+            var winningWord: String? = null
+            try {
+                winningWord = winningReader.readLine()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            if (winningWord == null) break
+            winningStringBuilder.append(winningWord).append("\n")
+        }
+        winningInputStream.close()
+
+        val winningCompleteString = winningStringBuilder.toString()
+        winningWordsList.addAll(winningCompleteString.split("\n"))
+        return winningWordsList
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        keyboardLayout= findViewById(R.id.keyboard_include)
+        keyboardLayout = findViewById(R.id.keyboard_include)
         helpButton = findViewById(R.id.icon_help)
         restartButton = findViewById(R.id.icon_restart)
 
-        helpButton.setOnClickListener{
+        helpButton.setOnClickListener {
             intent = Intent(this, HelpActivity::class.java)
             startActivity(intent)
         }
+
+        val winningWordsList: MutableList<String> = initiateWinningWords()
+        val dictionaryWords: MutableList<String> = initiateDictionaryWords()
+
     }
-    fun changeKeyColor(keyId: Int, colorId: Int){
-        keyboardLayout.findViewById<TextView>(keyId).setBackgroundColor(ResourcesCompat.getColor(resources, colorId, null))
+
+    fun changeKeyColor(keyId: Int, colorId: Int) {
+        keyboardLayout.findViewById<TextView>(keyId)
+            .setBackgroundColor(ResourcesCompat.getColor(resources, colorId, null))
     }
 }
